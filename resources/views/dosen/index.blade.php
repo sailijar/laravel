a9f79b696a35b7efebecc9bab99394ec952ac13c
sailijar <sailijar88@gmail.com>
2025-05-23 11:33:27 +0700

    update index

--- resources/views/dosen/index.blade.php
+++ resources/views/dosen/index.blade.php
@@ -5,7 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><a href="/dosen/tambah" class="btn btn-primary btn-sm">Tambah Data</a></div>
+                <div class="card-header">
+                    <div class="float-end"><a href="/dosen/tambah" class="btn btn-primary btn-sm"><i class="fa-solid fa-user-plus"></i> Tambah Data</a></div>
+
+                </div>
 
                 <div class="card-body">
 
@@ -20,17 +23,48 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                                <td>
-                                    <a href="" class="btn btn-warning btn-sm">detail</a>
-                                    <a href="" class="btn btn-info btn-sm">edit</a>
-                                    <a href="" class="btn btn-danger btn-sm">hapus</a>
-                                </td>
-                            </tr>
+                            @forelse ( $dosen as $data)
+                                <tr>
+                                    <th scope="row">{{$nomor++}}</th>
+                                    <td>{{$data->nidn}}</td>
+                                    <td>{{$data->nama}}</td>
+                                    <td>{{$data->email}}</td>
+                                    <td>
+                                        <a href="" class="btn btn-warning btn-sm"><i class="fa-solid fa-circle-info"></i></a>
+                                        <a href="/dosen/edit/{{$data->id}}" class="btn btn-info btn-sm"><i class="fa-solid fa-pen-to-square"></i></a>
+
+
+                                        <!-- Button trigger modal -->
+                                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                        <i class="fa-solid fa-trash"></i>
+                                        </button>
+
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Peringatan</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Yakin Data Dosen a.n. {{$data->nama}} ingin dihapus?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                                <button type="button" class="btn btn-danger">Hapus</button>
+                                            </div>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <th colspan="5" scope="row">Data Tidak Ada</th>
+                                </tr>
+                            @endforelse
+
 
                         </tbody>
                     </table>
